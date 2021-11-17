945f0be42065b8139a41c67c9a23a6f419d2d0dc
RahulGoyal03 <91531231+RahulGoyal03@users.noreply.github.com>
2021-11-17 22:51:15 +0530

    update app.js

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 const express = require("express");
 const UserData = require("./user")
+const data = require("./user.data");
+
 
 const app = express();
+app.use(express.json());
 
 app.get("/", (req, res) => {
     res.send("Welcome to my HomePage")
 })
 app.get("/user", (req, res) => {
     res.send(UserData)
+})
+app.get("/data", (req, res) => {
+    res.send(data)
 })
 
 app.listen(6969, function() {
